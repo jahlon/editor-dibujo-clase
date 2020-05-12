936a5889bfea3aa1b0b9a95597add51b534bdfed
Jesus Hincapie <jahlon@gmail.com>
2020-05-12 11:50:11 -0500

    Se agreggo el código para iniciar la aplicación

---
+++ editordibujo/__main__.py
@@ -0,0 +1,16 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication
+
+from editordibujo.ui.vista import MainWindowEditorDibujo
+
+
+def main():
+    app = QApplication(sys.argv)
+    win = MainWindowEditorDibujo()
+    win.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
